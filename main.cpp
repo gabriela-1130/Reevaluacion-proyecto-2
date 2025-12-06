ea02ce46a88ba73201296f82494c6362ba355a70
gabriela-1130 <a01714118@tec.mx>
2025-12-05 22:06:11 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Perro.h"
+#include "Gato.h"
+#include "Voluntario.h"
+#include "Adopcion.h"
+#include "Refugio.h"
+using namespace std;
+
+int main() {
+
+    Perro p1("Firulais", 3, "Sano", "Labrador");
+    Gato g1("Michi", 2, "Herido", true);
+    Voluntario v1("Angel", 12);
+
+    Refugio r1("Refugio Patitas");
+
+    r1.agregarAnimal(&p1);
+    r1.agregarAnimal(&g1);
+
+    r1.agregarVoluntario(v1);
+
+    r1.listarAnimales();
+    r1.listarVoluntarios();
+
+    Adopcion a1("Ana", &p1);
+    a1.registrarAdopcion();
+
+    return 0;
+}
